042fdfa8057c7e6193a60323fc94b2cd687ec038
Muhammad Saad <saadstudent.cs@gmail.com>
2024-07-14 10:46:13 +0500

    Added: res.SendFile

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
+// app.get('/', (req, res) => {
+//   res.send(
+//     `<section style="font-family: Arial, sans-serif; text-align: center; padding: 20px;">
+//       <h1 style="color: #4CAF50;">Hello, Code Wizard!</h1>
+//       <h3 style="color: #FF9800;">Got a moment?</h3>
+//       <p style="font-size: 1.2em;">Your API is not just running; it's sprinting like Usain Bolt!</p>
+//       <div style="margin-top: 20px;">
+//         <p style="font-size: 1.5em; font-weight: bold;">Everything is Awesome!</p>
+//         <p style="font-size: 1.2em;">💻✨🚀</p>
+//       </div>
+//       <footer style="margin-top: 30px; font-size: 0.8em; color: #9E9E9E;">
+//         <p>Need more magic? Explore the code and unleash your creativity!</p>
+//         <p>Happy coding, developer! 🎉</p>
+//       </footer>
+//     </section>`
+//   );
+// });
+
 app.get('/', (req, res) => {
-  res.send(
-    `<section style="font-family: Arial, sans-serif; text-align: center; padding: 20px;">
-      <h1 style="color: #4CAF50;">Hello, Code Wizard!</h1>
-      <h3 style="color: #FF9800;">Got a moment?</h3>
-      <p style="font-size: 1.2em;">Your API is not just running; it's sprinting like Usain Bolt!</p>
-      <div style="margin-top: 20px;">
-        <p style="font-size: 1.5em; font-weight: bold;">Everything is Awesome!</p>
-        <p style="font-size: 1.2em;">💻✨🚀</p>
-      </div>
-      <footer style="margin-top: 30px; font-size: 0.8em; color: #9E9E9E;">
-        <p>Need more magic? Explore the code and unleash your creativity!</p>
-        <p>Happy coding, developer! 🎉</p>
-      </footer>
-    </section>`
-  );
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
 app.listen(PORT, () => {
